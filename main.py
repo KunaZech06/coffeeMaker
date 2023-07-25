a1dae107b923a6d97ebae0a5c1c4d95b4531f103
kuna <ynwa0627@gmail.com>
2023-07-25 18:48:26 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+# TODO: 1. Prompt the user “What would you like? (espresso/latte/cappuccino):”. If report is typed, it should return
+#  all the available resources.
+
+# TODO: 2. Entering "off" should stop the program and turn off the machine.
+# TODO: 3. Check resources sufficient?
+# TODO: 4.
+
+
